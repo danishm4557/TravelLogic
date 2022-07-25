5bc15ba8339b0edee3e1594a7c5ec027f89ba6a1
Danish Mohiuddin <danishm4557@gmail.com>
2022-07-25 17:01:43 -0500

    Trying to get maps search to work again

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const places = express.Router()
-const Place = require('../models/placeModel')
-const compression = require('compression')
+const express = require("express");
+const places = express.Router();
+const Place = require("../models/placeModel");
+const compression = require("compression");
 
-places.use(compression())  // Data coming from Front-End too large. Using this to compress it.
+places.use(compression()); // Data coming from Front-End too large. Using this to compress it.
 
-places.get('/', (req, res) => {
-	Place.find({}, (error, foundPlace) => {
-	  if(error) {
-		res.status(400).json({ error: error.message})
-	  } else {
-		res.status(200).json(foundPlace)
-	  }
-	})
-  })
+places.get("/", (req, res) => {
+  Place.find({}, (error, foundPlace) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(200).json(foundPlace);
+    }
+  });
+});
 
-places.post('/', (req, res) => {
-	console.log(req.body);
-	Place.create(req.body, (error, createdPlaces) => {
-	  if(error) {
-		res.status(400).json({ error: error.message})
-	  } else {
-		res.flush()
-		console.log(createdPlaces);
-		res.status(200).json(createdPlaces)
-
-	  }
-	})
-  })
-  
+places.post("/", (req, res) => {
+  console.log(req.body);
+  Place.create(req.body, (error, createdPlaces) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.flush();
+      console.log(createdPlaces);
+      res.status(200).json(createdPlaces);
+    }
+  });
+});
 
 //   let placeFoundById = []
 // // Add ID as a parameter to the route    (/:id/:date)
@@ -44,23 +42,16 @@ places.post('/', (req, res) => {
 // 	}
 //   })
 
+places.delete("/:id", (req, res) => {
+  Place.findByIdAndDelete(req.params.id, (error, deletedPlan) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    } else if (deletedPlan === null) {
+      res.status(404).json({ message: "Plan not found" });
+    } else {
+      res.status(200).json({ message: "Plan with ID: " + req.params.id } + " ..DELETED");
+    }
+  });
+});
 
-places.delete('/:id', (req, res) => {
-	Place.findByIdAndDelete(req.params.id, (error, deletedPlan) => {
-		if (error) {
-			res.status(400).json({error: error.message})
-		} else if (deletedPlan === null) {
-			res.status(404).json({message: 'Plan not found'})
-		} else {
-			res.status(200).json({message: 'Plan with ID: ' + req.params.id} + ' ..DELETED')
-		}
-	})
-})
-
-
-
-
-
-
-
-module.exports = places
+module.exports = places;
